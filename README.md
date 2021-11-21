847d01f35b2964feffbd4bafeced687dfb3faa67
activivan <activivan@mail.de>
2021-11-21 23:42:08 +0100

    1st commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# Minetest Discord Webhook
+
+This is a simple Minetest mod, that sends Messages to Discord via a Webhook, whenever a user joins or leaves, the server starts or shuts down and on every chat message. With this mod you can easily connect your Mintest game to your Discord server.
+
+
+
+## Getting started
+
+### Install Minetest Mod
+
+To install this mod, you have two options:
+
+* Clone this repository into the `minetest/mods` directory by executing
+
+  ```shell
+  git clone https://github.com/activivan/mt-dcwebhook.git
+  ```
+
+* Download this repository as an Archive: https://github.com/activivan/mt-dcwebhook/archive/refs/heads/main.zip 
+
+  Extract the archive into the `minetest/mods` directory
+
+### Creating a Discord Webhook
+
+1. Go to your Discord server settings
+2. Go to "Integrations"
+3. Go to "Webhooks" and create a new Webhook
+4. Copy the URL of the Webhook to your clipboard
+
+### Configuring the mod
+
+5. Open `minetest.conf` and add your copied Discord Webhook URL like so:
+
+   ```conf
+   dcwebhook_url = https://discord.com/api/webhooks/blablabla
+   ```
+
+6. Optionally you can set the language of the messages with lang codes (currently available: English: `en`, German: `de` and Russian: `ru`)
+
+   ```conf
+   lang = de
+   ```
+
+### Add mod to http_mods
+
+As this mod uses the Minetest HTTP API to work, it has to be added to the `secure.http_mods` property in the `minetest.conf` file. Alternatively it can be added to `secure.trusted_mods`
+
+
+
+# License
+
+Copyright 2021 activivan <activivan.studios@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the  "Software"), to deal in the Software without restriction, including  without limitation the rights to use, copy, modify, merge, publish,  distribute, sublicense, and/or sell copies of the Software, and to  permit persons to whom the Software is furnished to do so, subject to  the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ init.lua
@@ -0,0 +1,130 @@
+-- Minetest Discord Webhook
+-- © 2021 activivan
+
+-- Please reade readme.md for more information
+
+local http = minetest.request_http_api()
+local conf = minetest.settings
+
+if not http then
+	minetest.log("error", "[Discord Webhook] Can not access HTTP API. Please add this mod to secure.http_mods to grant access")
+	return
+end
+
+if not conf:get("dcwebhook_url") then
+	minetest.log("error", "[Discord Webhook] Discord Webhook URL not set. Please set it in minetest.conf")
+	return
+end
+
+-- Just doing conf:get_bool("lang", "en") did not work for some reason
+if conf:get("lang") then
+	lang = conf:get("lang")
+else
+	lang = "en"
+end
+
+local function sendWebhook(data)
+	local json = minetest.write_json(data)
+
+	http.fetch({
+		url = conf:get("dcwebhook_url"),
+		method = "POST",
+		extra_headers = { "Content-Type: application/json" },
+		data = json
+	}, function()
+		-- doin nothin
+	end)
+end
+
+local texts = {
+	en = {
+		join1 = "Player **",
+		join2 = "** joined the game",
+		leave1 = "Player **",
+		leave2 = "** left the game",
+		shutdown = "Server shut down",
+		start = "Server started"
+	},
+	de = {
+		join1 = "Spieler **",
+		join2 = "** dem Spiel beigetreten",
+		leave1 = "Spieler **",
+		leave2 = "** hat das Spiel verlassen",
+		shutdown = "Server heruntergefahren",
+		start = "Server gestartet"
+	},
+	ru = {
+		join1 = "Игрок **",
+		join2 = "** вошел в игру",
+		leave1 = "Игрок **",
+		leave2 = "** вышел из игры",
+		shutdown = "Сервер выключен",
+		start = "Сервер запущен"
+	}
+}
+
+minetest.register_on_joinplayer(function(player)
+    local name = player:get_player_name()
+
+	local data = {
+		content = nil,
+		embeds = {
+			{
+				description = texts[lang].join1 .. name .. texts[lang].join2,
+				color = 5763719
+			}
+		}
+	}
+
+	sendWebhook(data)
+end)
+
+minetest.register_on_leaveplayer(function(player)
+    local name = player:get_player_name()
+
+	local data = {
+		content = nil,
+		embeds = {
+			{
+				description = texts[lang].leave1 .. name .. texts[lang].leave2,
+				color = 15548997
+			}
+		}
+	}
+
+	sendWebhook(data)
+end)
+
+minetest.register_on_chat_message(function(name, message)
+	sendWebhook({ content = "<**" .. name .. "**>  " .. message })
+end)
+
+minetest.register_on_shutdown(function() 
+	local data = {
+		content = nil,
+		embeds = {
+			{
+				title = texts[lang].shutdown
+			}
+		}
+	}
+
+	sendWebhook(data)
+end)
+
+local function startupMessage()
+	local data = {
+		content = nil,
+		embeds = {
+			{
+				title = texts[lang].start,
+				description = minetest.get_server_status(),
+				color = 5793266
+			}
+		}
+	}
+
+	sendWebhook(data)
+end
+
+startupMessage()
